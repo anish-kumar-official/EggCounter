37aacac1fbdec780c9a710f70f80906e45eeec08
Anish kumar <anish.bplofficial@gmail.com>
2022-04-08 11:41:29 +0530

    game 2 startup page done

--- app/src/main/java/com/example/eggcounter/Fragments/Game2.kt
+++ app/src/main/java/com/example/eggcounter/Fragments/Game2.kt
@@ -6,15 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.eggcounter.R
+import com.example.eggcounter.databinding.FragmentGame1Binding
+import com.example.eggcounter.databinding.FragmentGame2Binding
 
 
 class Game2 : Fragment() {
+    private var _binding: FragmentGame2Binding?=null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game2, container, false)
+        _binding =  FragmentGame2Binding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        return view
     }
 }
